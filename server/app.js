2bee6128daf84bb9f8b077c94f2adebc4ef39721
Ray DJ <raydjyoti@gmail.com>
2024-11-09 22:45:25 +0530

    Added cors domain

--- server/app.js
+++ server/app.js
@@ -5,8 +5,15 @@ import bodyParser from "body-parser";
 import plagiarismRoutes from "./routes/plagiarismRoutes.js";
 
 const app = express();
+const corsOptions = {
+    origin: ['https://rayanswersai.xyz', 'https://api.rayanswersai.xyz', 'api.rayanswersai.xyz', 'rayanswersai.xyz'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
+
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: '1mb' }));
 
